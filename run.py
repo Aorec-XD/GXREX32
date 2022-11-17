512415ba9d5064773c7f7c7f633be8cd28c45c30
AOREC <98394423+Aorec-XD@users.noreply.github.com>
2022-11-17 07:41:03 +0700

    Update run.py

--- run.py
+++ run.py
@@ -1,9 +1,10 @@
+
 import os
 try:
  os.mkdir('result')
 except:pass
 if __name__ == "__main__":
         try:
-                __import__("gxrex").lisensi()
+                __import__("gassken").AORECGANTENG()
         except Exception as e:
                 exit(str(e))
